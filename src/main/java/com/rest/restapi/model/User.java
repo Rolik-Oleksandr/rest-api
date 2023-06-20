049002c3b4f2f8e0997440cfe1d278b3ea6ef778
richn <rolik.alexx@gmail.com>
2023-06-20 21:36:57 +0300

    implement basic entities

---
+++ src/main/java/com/rest/restapi/model/User.java
@@ -0,0 +1,33 @@
+package com.rest.restapi.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User extends BaseEntity {
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+        joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+        inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    private List<Role> roles;
+}
