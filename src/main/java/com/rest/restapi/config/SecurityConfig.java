c9ae930c10f2351460d37890b0e77e6faef8f72f
richn <rolik.alexx@gmail.com>
2023-06-28 18:22:59 +0300

    little changes

--- src/main/java/com/rest/restapi/config/SecurityConfig.java
+++ src/main/java/com/rest/restapi/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.rest.restapi.config;
 
+import com.rest.restapi.security.jwt.JwtConfigurer;
+import com.rest.restapi.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
--- src/main/java/com/rest/restapi/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/rest/restapi/security/jwt/JwtTokenProvider.java
@@ -91,7 +91,6 @@ public class JwtTokenProvider {
 
     private List<String> getRoleNames(List<Role> userRoles) {
         List<String> result = new ArrayList<>();
-
         userRoles.forEach(role -> {
             result.add(role.getName());
         });
